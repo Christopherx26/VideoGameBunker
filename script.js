967af73aa3326dfd4b8fb1217e2725a97bafc999
Christopher Castaneda <40323128+Christopherx26@users.noreply.github.com>
2019-08-28 14:45:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+body {
+    background: url("https://raw.githubusercontent.com/rjpena88/VideoGameBunker/master/images/bg.jpg") no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+	color:#fff;
+  	background-color:#333;
+}
